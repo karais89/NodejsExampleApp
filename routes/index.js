029f5d33863cca0452665c00121434197c6ccc0e
totuworld <totuworld@gmail.com>
2016-04-23 06:37:15 +0900

    redis 모듈 추가 및 index.js 업데이트
    index.js는 redis를 사용하도록 업데이트

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+'use strict'
+
+const express = require('express');
 var router = express.Router();
+
+const redis = require('redis');
+
+console.log(`${process.env.REDIS_PORT_6379_TCP_ADDR}:${process.env.REDIS_PORT_6379_TCP_PORT}`);
+
+//여기서 사용된 redis는 Docker에서 redis란 이름으로 연결되는 컨테이너를 뜻한다. 곧 host주소이다.
+let client = redis.createClient('6379', 'redis');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ACS' });
+  client.incr('counter', function(err, counter) {
+    if(err) return next(err);
+    res.render('index', { title: 'ACS' , message:`This page has been viewed ${counter} times!`});
+  });
 });
 
 module.exports = router;
